0fc7ed95c0008d65da8a602d99e3bd67fb956144
K-MHOON <azmria117@gmail.com>
2024-12-30 23:44:36 +0900

    fix : OAS, config, 의존성 수정

--- app/src/test/java/com/kmhoon/app/eCommerceApplicationTests.java
+++ app/src/test/java/com/kmhoon/app/eCommerceApplicationTests.java
@@ -1,9 +1,13 @@
 package com.kmhoon.app;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureDataJpa;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@AutoConfigureDataJpa
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class eCommerceApplicationTests {
 
 	@Test
