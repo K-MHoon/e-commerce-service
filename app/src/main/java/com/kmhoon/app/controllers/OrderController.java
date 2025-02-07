cc80e7cb527f2d3b61b8059560e00ae08d8b7a24
K-MHOON <azmria117@gmail.com>
2025-02-07 23:17:04 +0900

    feat : 주문 컨트롤러 추가

---
+++ app/src/main/java/com/kmhoon/app/controllers/OrderController.java
@@ -0,0 +1,51 @@
+package com.kmhoon.app.controllers;
+
+import com.kmhoon.app.OrderApi;
+import com.kmhoon.app.hateoas.OrderRepresentationModelAssembler;
+import com.kmhoon.app.model.NewOrder;
+import com.kmhoon.app.model.Order;
+import com.kmhoon.app.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.NativeWebRequest;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.springframework.http.ResponseEntity.notFound;
+import static org.springframework.http.ResponseEntity.ok;
+
+@RestController
+@Slf4j
+@RequiredArgsConstructor
+public class OrderController implements OrderApi {
+
+    private final OrderRepresentationModelAssembler assembler;
+    private final OrderService service;
+
+
+    @Override
+    public ResponseEntity<Order> addOrder(NewOrder newOrder) {
+        return service.addOrder(newOrder)
+                .map(assembler::toModel)
+                .map(ResponseEntity::ok)
+                .orElse(notFound().build());
+    }
+
+    @Override
+    public ResponseEntity<List<Order>> getOrdersByCustomerId(String customerId) {
+        return ok(assembler.toListModel(service.getOrdersByCustomerId(customerId)));
+    }
+
+    @Override
+    public ResponseEntity<Order> getByOrderId(String id) {
+        return service.getByOrderId(id)
+                .map(assembler::toModel)
+                .map(ResponseEntity::ok)
+                .orElse(notFound().build());
+    }
+
+
+}
