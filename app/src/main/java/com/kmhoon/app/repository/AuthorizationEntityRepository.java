46114e0b0c53b26092dc231880b94cf6a3597865
K-MHOON <azmria117@gmail.com>
2025-02-24 23:24:06 +0900

    feat : Address, Authorization ReactiveCrudRepository 적용

--- app/src/main/java/com/kmhoon/app/repository/AuthorizationEntityRepository.java
+++ app/src/main/java/com/kmhoon/app/repository/AuthorizationEntityRepository.java
@@ -1,9 +1,9 @@
 package com.kmhoon.app.repository;
 
 import com.kmhoon.app.entity.AuthorizationEntity;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 
 import java.util.UUID;
 
-public interface AuthorizationEntityRepository extends CrudRepository<AuthorizationEntity, UUID> {
+public interface AuthorizationEntityRepository extends ReactiveCrudRepository<AuthorizationEntity, UUID> {
 }
