1bcb9679f5c9a3cf20cf0ca42fa80d3826101fb4
K-MHOON <azmria117@gmail.com>
2025-02-25 23:18:50 +0900

    feat : Card, Cart ReactiveCrudRepository 적용

--- app/src/main/java/com/kmhoon/app/repository/CardRepository.java
+++ app/src/main/java/com/kmhoon/app/repository/CardRepository.java
@@ -1,9 +1,9 @@
 package com.kmhoon.app.repository;
 
 import com.kmhoon.app.entity.CardEntity;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 
 import java.util.UUID;
 
-public interface CardRepository extends CrudRepository<CardEntity, UUID> {
+public interface CardRepository extends ReactiveCrudRepository<CardEntity, UUID> {
 }
--- app/src/main/java/com/kmhoon/app/repository/CartRepository.java
+++ app/src/main/java/com/kmhoon/app/repository/CartRepository.java
@@ -1,14 +1,14 @@
 package com.kmhoon.app.repository;
 
 import com.kmhoon.app.entity.CartEntity;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.r2dbc.repository.Query;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import reactor.core.publisher.Mono;
 
-import java.util.Optional;
 import java.util.UUID;
 
-public interface CartRepository extends CrudRepository<CartEntity, UUID> {
+public interface CartRepository extends ReactiveCrudRepository<CartEntity, UUID> {
 
-    @Query("select c from CartEntity c join c.user u where u.id = ?1")
-    Optional<CartEntity> findByCustomerId(UUID customerId);
+    @Query("select c.* from ecomm.cart c join ecomm.user u on c.user_id=u.id where u.id = :customerId")
+    Mono<CartEntity> findByCustomerId(UUID customerId);
 }
