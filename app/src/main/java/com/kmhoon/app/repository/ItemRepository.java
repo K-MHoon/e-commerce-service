2b0337ab418f0871a8be3d2f9e3c16bfbccc1d8f
K-MHOON <azmria117@gmail.com>
2025-02-26 23:43:52 +0900

    feat : Item 엔터티 ReactiveCrudRepository 적용

--- app/src/main/java/com/kmhoon/app/repository/ItemRepository.java
+++ app/src/main/java/com/kmhoon/app/repository/ItemRepository.java
@@ -1,20 +1,25 @@
 package com.kmhoon.app.repository;
 
 import com.kmhoon.app.entity.ItemEntity;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.r2dbc.repository.Query;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.util.List;
 import java.util.UUID;
 
-public interface ItemRepository extends CrudRepository<ItemEntity, UUID> {
+public interface ItemRepository extends ReactiveCrudRepository<ItemEntity, UUID> {
 
-    @Query(value = "select i.* from eshop.cart c, eshop.item i, eshop.user u, eshop.cart_item ci where u.id = :customerId and c.user_id=u.id and c.id=ci.cart_id and i.id=ci.item_id",
-    nativeQuery = true)
-    Iterable<ItemEntity> findByCustomerId(String customerId);
+    @Query("select i.* from ecomm.cart c, ecomm.item i, ecomm.user u, ecomm.cart_item ci where u.id = :customerId and c.user_id=u.id and c.id=ci.cart_id and i.id=ci.item_id")
+    Flux<ItemEntity> findByCustomerId(UUID customerId);
 
-    @Modifying
-    @Query(value = "delete from eshop.cart_item where item_id in (:ids) and cart_id = :cartId", nativeQuery = true)
-    void deleteCartItemJoinById(List<UUID> ids, UUID cartId);
+    @Query("delete from ecomm.cart_item where item_id in (:ids) and cart_id = :cartId")
+    Mono<Void> deleteCartItemJoinById(List<UUID> ids, UUID cartId);
+
+    @Query("delete from ecomm.item where id in (:ids)")
+    Mono<Void> deleteByIds(List<UUID> ids);
+
+    @Query("insert into ecomm.cart_item(cart_id, item_id) values(:cartId, :itemId)")
+    Mono<Void> saveMapping(UUID cartId, UUID itemId);
 }
