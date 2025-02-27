eeb76accafd39cef65ba52d8dbf7d5dfdb3086d3
K-MHOON <azmria117@gmail.com>
2025-02-27 23:21:56 +0900

    feat : OrderItem, Payment 엔터티 ReactiveCrudRepository 적용

--- app/src/main/java/com/kmhoon/app/repository/OrderItemRepository.java
+++ app/src/main/java/com/kmhoon/app/repository/OrderItemRepository.java
@@ -2,8 +2,9 @@ package com.kmhoon.app.repository;
 
 import com.kmhoon.app.entity.OrderItemEntity;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 
 import java.util.UUID;
 
-public interface OrderItemRepository extends CrudRepository<OrderItemEntity, UUID> {
+public interface OrderItemRepository extends ReactiveCrudRepository<OrderItemEntity, UUID> {
 }
--- app/src/main/java/com/kmhoon/app/repository/PaymentRepository.java
+++ app/src/main/java/com/kmhoon/app/repository/PaymentRepository.java
@@ -1,9 +1,9 @@
 package com.kmhoon.app.repository;
 
 import com.kmhoon.app.entity.PaymentEntity;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 
 import java.util.UUID;
 
-public interface PaymentRepository extends CrudRepository<PaymentEntity, UUID> {
+public interface PaymentRepository extends ReactiveCrudRepository<PaymentEntity, UUID> {
 }
