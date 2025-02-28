739d897f73c051b44bb997ce3cf28d693fefbb08
K-MHOON <azmria117@gmail.com>
2025-02-28 23:41:01 +0900

    feat : Product, Shipment 엔터티 ReactiveCrudRepository 적용

--- app/src/main/java/com/kmhoon/app/repository/ProductRepository.java
+++ app/src/main/java/com/kmhoon/app/repository/ProductRepository.java
@@ -1,9 +1,9 @@
 package com.kmhoon.app.repository;
 
 import com.kmhoon.app.entity.ProductEntity;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 
 import java.util.UUID;
 
-public interface ProductRepository extends CrudRepository<ProductEntity, UUID> {
+public interface ProductRepository extends ReactiveCrudRepository<ProductEntity, UUID> {
 }
--- app/src/main/java/com/kmhoon/app/repository/ShipmentRepository.java
+++ app/src/main/java/com/kmhoon/app/repository/ShipmentRepository.java
@@ -1,9 +1,14 @@
 package com.kmhoon.app.repository;
 
 import com.kmhoon.app.entity.ShipmentEntity;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.r2dbc.repository.Query;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import reactor.core.publisher.Flux;
 
 import java.util.UUID;
 
-public interface ShipmentRepository extends CrudRepository<ShipmentEntity, UUID> {
+public interface ShipmentRepository extends ReactiveCrudRepository<ShipmentEntity, UUID> {
+
+    @Query("select s.* from ecomm.order o, ecomm.shipment s where o.shipment_id = s.id and o.id = :id")
+    Flux<ShipmentEntity> getShipmentByOrderId(UUID id);
 }
