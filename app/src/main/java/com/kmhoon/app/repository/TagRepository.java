52f690890390fa34ff4b5476485cf855d41afb32
K-MHOON <azmria117@gmail.com>
2025-03-01 20:57:27 +0900

    feat : Tag, User 엔터티 ReactiveCrudRepository 적용

--- app/src/main/java/com/kmhoon/app/repository/TagRepository.java
+++ app/src/main/java/com/kmhoon/app/repository/TagRepository.java
@@ -1,9 +1,14 @@
 package com.kmhoon.app.repository;
 
 import com.kmhoon.app.entity.TagEntity;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.r2dbc.repository.Query;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import reactor.core.publisher.Flux;
 
 import java.util.UUID;
 
-public interface TagRepository extends CrudRepository<TagEntity, UUID> {
+public interface TagRepository extends ReactiveCrudRepository<TagEntity, UUID> {
+
+    @Query("select t.* from ecomm.proudct p, ecomm.tag t, ecom.product_tag pt where p.id = :id and p.id = pt.product_id and t.id = pt.tag_id")
+    Flux<TagEntity> findTagsByProductId(UUID id);
 }
