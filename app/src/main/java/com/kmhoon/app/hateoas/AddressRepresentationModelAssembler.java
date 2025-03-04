99151442b2ee30f3bacedf2ffd58bff6bc5d1f1b
K-MHOON <azmria117@gmail.com>
2025-03-04 23:42:04 +0900

    feat : Address Model Assembler 추가

--- app/src/main/java/com/kmhoon/app/hateoas/AddressRepresentationModelAssembler.java
+++ app/src/main/java/com/kmhoon/app/hateoas/AddressRepresentationModelAssembler.java
@@ -1,39 +1,60 @@
 package com.kmhoon.app.hateoas;
 
-import com.kmhoon.app.controllers.AddressController;
 import com.kmhoon.app.entity.AddressEntity;
 import com.kmhoon.app.model.Address;
+import org.apache.logging.log4j.util.Strings;
 import org.springframework.beans.BeanUtils;
-import org.springframework.hateoas.server.mvc.RepresentationModelAssemblerSupport;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.server.reactive.ReactiveRepresentationModelAssembler;
+import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
-import java.util.List;
 import java.util.Objects;
-import java.util.stream.StreamSupport;
-
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+import java.util.concurrent.atomic.AtomicReference;
 
 @Component
-public class AddressRepresentationModelAssembler extends RepresentationModelAssemblerSupport<AddressEntity, Address> {
+public class AddressRepresentationModelAssembler implements ReactiveRepresentationModelAssembler<AddressEntity, Address>, HateoasSupport {
 
-    public AddressRepresentationModelAssembler() {
-        super(AddressController.class, Address.class);
-    }
+    private static String serverUri = null;
 
     @Override
-    public Address toModel(AddressEntity entity) {
-        Address resource = createModelWithId(entity.getId(), entity);
-        BeanUtils.copyProperties(entity, resource);
-        resource.setId(entity.getId().toString());
-
-        return resource.add(linkTo(methodOn(AddressController.class).getAddressesById(entity.getId().toString())).withSelfRel());
+    public Mono<Address> toModel(AddressEntity entity, ServerWebExchange exchange) {
+        return Mono.just(entityToModel(entity, exchange));
     }
 
-    public List<Address> toListModel(Iterable<AddressEntity> entities) {
-        if(Objects.isNull(entities)) {
-            return List.of();
+    private String getServerUri(@Nullable ServerWebExchange exchange){
+        if(Strings.isBlank(serverUri)) {
+            serverUri = getUriComponentsBuilder(exchange).toUriString();
         }
-        return StreamSupport.stream(entities.spliterator(), false).map(this::toModel).toList();
+        return serverUri;
+    }
+
+    public Address entityToModel(AddressEntity entity, ServerWebExchange exchange) {
+        Address resource = new Address();
+        if(Objects.isNull(entity)) {
+            return resource;
+        }
+        BeanUtils.copyProperties(entity, resource);
+        resource.setId(entity.getId().toString());
+        String serverUri = getServerUri(exchange);
+        resource.add(Link.of(String.format("%s/api/v1/addresses", serverUri)).withRel("addresses"));
+        resource.add(Link.of(String.format("%s/api/v1/addresses/%s", serverUri, entity.getId())).withSelfRel());
+        return resource;
+    }
+
+    public Address getModel(Mono<Address> m) {
+        AtomicReference<Address> model = new AtomicReference<>();
+        m.cache().subscribe(model::set);
+        return model.get();
+    }
+
+    public Flux<Address> toListModel(Flux<AddressEntity> entities, ServerWebExchange exchange) {
+        if (Objects.isNull(entities)) {
+            return Flux.empty();
+        }
+        return Flux.from(entities.map(entity -> entityToModel(entity, exchange)));
     }
 }
