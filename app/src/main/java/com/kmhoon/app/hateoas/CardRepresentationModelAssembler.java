8d181dc39f1d743d89205ec8b15ccc4355d2e01f
K-MHOON <azmria117@gmail.com>
2025-03-05 23:42:24 +0900

    fix : Card Model Assembler Reactive 형식으로 수정

--- app/src/main/java/com/kmhoon/app/hateoas/CardRepresentationModelAssembler.java
+++ app/src/main/java/com/kmhoon/app/hateoas/CardRepresentationModelAssembler.java
@@ -3,42 +3,67 @@ package com.kmhoon.app.hateoas;
 import com.kmhoon.app.controllers.CardController;
 import com.kmhoon.app.entity.CardEntity;
 import com.kmhoon.app.model.Card;
+import org.apache.logging.log4j.util.Strings;
 import org.springframework.beans.BeanUtils;
+import org.springframework.hateoas.Link;
 import org.springframework.hateoas.server.mvc.RepresentationModelAssemblerSupport;
+import org.springframework.hateoas.server.reactive.ReactiveRepresentationModelAssembler;
+import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.util.List;
 import java.util.Objects;
+import java.util.concurrent.atomic.AtomicReference;
 import java.util.stream.StreamSupport;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @Component
-public class CardRepresentationModelAssembler extends RepresentationModelAssemblerSupport<CardEntity, Card> {
+public class CardRepresentationModelAssembler implements ReactiveRepresentationModelAssembler<CardEntity, Card>, HateoasSupport {
 
-    public CardRepresentationModelAssembler() {
-        super(CardController.class, Card.class);
+    private static String serverUri = null;
+
+    private String getServerUri(@Nullable ServerWebExchange exchange) {
+        if (Strings.isBlank(serverUri)) {
+            serverUri = getUriComponentsBuilder(exchange).toUriString();
+        }
+        return serverUri;
     }
 
     @Override
-    public Card toModel(CardEntity entity) {
-        String uid = Objects.isNull(entity.getUser()) ? null : entity.getUser().getId().toString();
-        Card resource = createModelWithId(entity.getId(), entity);
-        BeanUtils.copyProperties(entity, resource);
-        resource.id(entity.getId().toString())
-                .cardNumber(entity.getNumber())
-                .cvv(entity.getCvv())
-                .expires(entity.getExpires())
-                .userId(uid);
-
-        return resource.add(linkTo(methodOn(CardController.class).getCardById(entity.getId().toString())).withSelfRel());
+    public Mono<Card> toModel(CardEntity entity, ServerWebExchange exchange) {
+        return null;
     }
 
-    public List<Card> toListModel(Iterable<CardEntity> entities) {
-        if(Objects.isNull(entities)) {
-            return List.of();
+    public Card entityToModel(CardEntity entity, ServerWebExchange exchange) {
+        Card resource = new Card();
+        if(Objects.isNull(entity)){
+            return resource;
         }
-        return StreamSupport.stream(entities.spliterator(), false).map(this::toModel).toList();
+        BeanUtils.copyProperties(entity, resource);
+        resource.setId(Objects.isNull(entity.getId())? entity.getId().toString() : "");
+        resource.setCardNumber(entity.getNumber());
+        resource.setUserId(entity.getUserId().toString());
+        String serverUri = getServerUri(exchange);
+        resource.add(Link.of(String.format("%s/api/v1/cards", serverUri)).withRel("cards"));
+        resource.add(Link.of(String.format("%s/api/v1/cards/%s", serverUri, entity.getId())).withRel("self"));
+        return resource;
+    }
+
+    public Card getModel(Mono<Card> m, ServerWebExchange exchange) {
+        AtomicReference<Card> model = new AtomicReference<>();
+        m.cache().subscribe(model::set);
+        return model.get();
+    }
+
+    public Flux<Card> toListModel(Flux<CardEntity> entities, ServerWebExchange exchange) {
+        if (Objects.isNull(entities)) {
+            return Flux.empty();
+        }
+        return Flux.from(entities.map(e -> entityToModel(e, exchange)));
     }
 }
