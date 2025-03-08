f7d3a187056c885f8b297e084e3c02f83c23c2c7
K-MHOON <azmria117@gmail.com>
2025-03-09 00:02:34 +0900

    fix : Payment Model Assembler Fixed By Reactive

--- app/src/main/java/com/kmhoon/app/hateoas/PaymentRepresentationModelAssembler.java
+++ app/src/main/java/com/kmhoon/app/hateoas/PaymentRepresentationModelAssembler.java
@@ -1,39 +1,47 @@
 package com.kmhoon.app.hateoas;
 
-import com.kmhoon.app.controllers.PaymentController;
 import com.kmhoon.app.entity.PaymentEntity;
 import com.kmhoon.app.model.Payment;
 import org.springframework.beans.BeanUtils;
-import org.springframework.hateoas.server.mvc.RepresentationModelAssemblerSupport;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.server.reactive.ReactiveRepresentationModelAssembler;
 import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
-import java.util.List;
 import java.util.Objects;
-import java.util.stream.StreamSupport;
-
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+import java.util.concurrent.atomic.AtomicReference;
 
 @Component
-public class PaymentRepresentationModelAssembler extends RepresentationModelAssemblerSupport<PaymentEntity, Payment> {
-
-    public PaymentRepresentationModelAssembler() {
-        super(PaymentController.class, Payment.class);
-    }
+public class PaymentRepresentationModelAssembler implements ReactiveRepresentationModelAssembler<PaymentEntity, Payment>, HateoasSupport {
 
     @Override
-    public Payment toModel(PaymentEntity entity) {
-        Payment resource = createModelWithId(entity.getId(), entity);
-        BeanUtils.copyProperties(entity, resource);
-        resource.setId(entity.getId().toString());
-
-        return resource.add(linkTo(methodOn(PaymentController.class).getOrdersPaymentAuthorization(entity.getId().toString())).withSelfRel());
+    public Mono<Payment> toModel(PaymentEntity entity, ServerWebExchange exchange) {
+        return Mono.just(entityToModel(entity));
     }
 
-    public List<Payment> toListModel(Iterable<PaymentEntity> entities) {
-        if(Objects.isNull(entities)) {
-            return List.of();
+    public Payment entityToModel(PaymentEntity entity) {
+        Payment resource = new Payment();
+        if(Objects.isNull(entity)) {
+            return resource;
         }
-        return StreamSupport.stream(entities.spliterator(), false).map(this::toModel).toList();
+        BeanUtils.copyProperties(entity, resource);
+        resource.add(Link.of("/api/v1/payments").withRel("payments"));
+        resource.add(Link.of("/api/v1/payments/%s" + entity.getId()).withSelfRel());
+        return resource;
+    }
+
+    public Payment getModel(Mono<Payment> m) {
+        AtomicReference<Payment> model = new AtomicReference<>();
+        m.cache().subscribe(model::set);
+        return model.get();
+    }
+
+    public Flux<Payment> toListModel(Flux<PaymentEntity> entities) {
+        if(Objects.isNull(entities)) {
+            return Flux.empty();
+        }
+        return Flux.from(entities.map(this::entityToModel));
     }
 }
