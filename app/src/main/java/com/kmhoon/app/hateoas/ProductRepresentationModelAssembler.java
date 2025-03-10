51630eddba9472305d5270ad41ec3de43765a814
K-MHOON <azmria117@gmail.com>
2025-03-10 22:30:54 +0900

    fix : 상품 관련 Model Assembler Fixed By Reactive

--- app/src/main/java/com/kmhoon/app/hateoas/ProductRepresentationModelAssembler.java
+++ app/src/main/java/com/kmhoon/app/hateoas/ProductRepresentationModelAssembler.java
@@ -1,51 +1,65 @@
 package com.kmhoon.app.hateoas;
 
-import com.kmhoon.app.controllers.ProductController;
 import com.kmhoon.app.entity.ProductEntity;
 import com.kmhoon.app.entity.TagEntity;
 import com.kmhoon.app.model.Product;
 import com.kmhoon.app.model.Tag;
+import org.apache.logging.log4j.util.Strings;
 import org.springframework.beans.BeanUtils;
-import org.springframework.hateoas.server.mvc.RepresentationModelAssemblerSupport;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.server.reactive.ReactiveRepresentationModelAssembler;
+import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.util.List;
 import java.util.Objects;
-import java.util.stream.StreamSupport;
-
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @Component
-public class ProductRepresentationModelAssembler extends RepresentationModelAssemblerSupport<ProductEntity, Product> {
+public class ProductRepresentationModelAssembler implements ReactiveRepresentationModelAssembler<ProductEntity, Product>, HateoasSupport {
 
-    public ProductRepresentationModelAssembler() {
-        super(ProductController.class, Product.class);
+    private static String serverUri = null;
+
+    private String getServerUri(@Nullable ServerWebExchange exchange) {
+        if (Strings.isBlank(serverUri)) {
+            serverUri = getUriComponentsBuilder(exchange).toUriString();
+        }
+        return serverUri;
     }
 
     @Override
-    public Product toModel(ProductEntity entity) {
-        String pid = entity.getId().toString();
-        Product resource = createModelWithId(entity.getId(), entity);
-        BeanUtils.copyProperties(entity, resource);
-        resource.setId(pid);
-        resource.setTag(entity.getTags().stream().map(this::TagEntityToTag).toList());
-        resource.add(linkTo(methodOn(ProductController.class).getProduct(pid)).withSelfRel());
-        resource.add(linkTo(methodOn(ProductController.class).queryProducts(null, null, 1, 10)).withRel("products"));
+    public Mono<Product> toModel(ProductEntity entity, ServerWebExchange exchange) {
+        return Mono.just(entityToModel(entity, exchange));
+    }
 
+    public Product entityToModel(ProductEntity entity, ServerWebExchange exchange) {
+        Product resource = new Product();
+        if(Objects.isNull(entity)) {
+            return resource;
+        }
+        BeanUtils.copyProperties(entity, resource);
+        resource.setId(entity.getId().toString());
+        resource.setTag(tagFromEntities(entity.getTags()));
+        resource.add(Link.of("/api/v1/products").withRel("products"));
+        resource.add(Link.of(String.format("/api/v1/products/%s", entity.getId())).withSelfRel());
         return resource;
     }
 
-    private Tag TagEntityToTag(TagEntity t) {
-        return new Tag().id(t.getId().toString()).name(t.getName());
+    public List<Tag> tagFromEntities(List<TagEntity> tags) {
+        return tags.stream().map(t -> {
+            Tag tag = new Tag();
+            BeanUtils.copyProperties(t, tag);
+            tag.setId(t.getId().toString());
+            return tag;
+        }).toList();
     }
 
-    public List<Product> toListModel(Iterable<ProductEntity> entities) {
+    public Flux<Product> toListModel(Flux<ProductEntity> entities, ServerWebExchange exchange) {
         if (Objects.isNull(entities)) {
-            return List.of();
+            return Flux.empty();
         }
-        return StreamSupport.stream(entities.spliterator(), false)
-                .map(this::toModel)
-                .toList();
+        return Flux.from(entities.map(e -> entityToModel(e, exchange)));
     }
 }
