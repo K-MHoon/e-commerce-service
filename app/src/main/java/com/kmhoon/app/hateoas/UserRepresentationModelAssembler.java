2cd900ddcd2c1ce0bb8c15b5f9eb0b771a67346a
K-MHOON <azmria117@gmail.com>
2025-03-12 23:38:17 +0900

    fix : 사용자 Model Assembler Fixed By Reactive

--- app/src/main/java/com/kmhoon/app/hateoas/UserRepresentationModelAssembler.java
+++ app/src/main/java/com/kmhoon/app/hateoas/UserRepresentationModelAssembler.java
@@ -5,12 +5,20 @@ import com.kmhoon.app.entity.ShipmentEntity;
 import com.kmhoon.app.entity.UserEntity;
 import com.kmhoon.app.model.Shipment;
 import com.kmhoon.app.model.User;
+import org.apache.logging.log4j.util.Strings;
 import org.springframework.beans.BeanUtils;
+import org.springframework.hateoas.Link;
 import org.springframework.hateoas.server.mvc.RepresentationModelAssemblerSupport;
+import org.springframework.hateoas.server.reactive.ReactiveRepresentationModelAssembler;
+import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.util.List;
 import java.util.Objects;
+import java.util.concurrent.atomic.AtomicReference;
 import java.util.stream.StreamSupport;
 
 import static java.util.stream.Collectors.toList;
@@ -18,29 +26,46 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @Component
-public class UserRepresentationModelAssembler extends RepresentationModelAssemblerSupport<UserEntity, User> {
+public class UserRepresentationModelAssembler implements ReactiveRepresentationModelAssembler<UserEntity, User>, HateoasSupport {
 
-    public UserRepresentationModelAssembler() {
-        super(CustomerController.class, User.class);
+    private static String serverUri = null;
+
+    private String getServerUri(@Nullable ServerWebExchange exchange) {
+        if(Strings.isBlank(serverUri)) {
+            serverUri = getUriComponentsBuilder(exchange).toString();
+        }
+        return serverUri;
     }
 
     @Override
-    public User toModel(UserEntity entity) {
-        User resource = createModelWithId(entity.getId(), entity);
+    public Mono<User> toModel(UserEntity entity, ServerWebExchange exchange) {
+        return Mono.just(entityToModel(entity, exchange));
+    }
+
+    public User entityToModel(UserEntity entity, ServerWebExchange exchange) {
+        User resource = new User();
+        if(Objects.isNull(entity)) {
+            return resource;
+        }
         BeanUtils.copyProperties(entity, resource);
         resource.setId(entity.getId().toString());
-
-        resource.add(linkTo(methodOn(CustomerController.class).getCustomerById(entity.getId().toString())).withSelfRel());
-        resource.add(linkTo(methodOn(CustomerController.class).getAllCustomers()).withRel("customers"));
-        resource.add(linkTo(methodOn(CustomerController.class).getAddressesByCustomerId(entity.getId().toString())).withRel("self_addresses"));
-
+        String serverUri = getServerUri(exchange);
+        resource.add(Link.of(String.format("%s/api/v1/customers", serverUri)).withRel("customers"));
+        resource.add(Link.of(String.format("%s/api/v1/customers/%s", serverUri, entity.getId())).withSelfRel());
+        resource.add(Link.of(String.format("%s/api/v1/customers/%s/addresses", serverUri, entity.getId())).withRel("self_addresses"));
         return resource;
     }
 
-    public List<User> toListModel(Iterable<UserEntity> entities) {
-        if (Objects.isNull(entities)) {
-            return List.of();
+    public User getModel(Mono<User> m, ServerWebExchange exchange) {
+        AtomicReference<User> model = new AtomicReference<>();
+        m.cache().subscribe(model::set);
+        return model.get();
+    }
+
+    public Flux<User> toListModel(Flux<UserEntity> entities, ServerWebExchange exchange) {
+        if(Objects.isNull(entities)) {
+            return Flux.empty();
         }
-        return StreamSupport.stream(entities.spliterator(), false).map(this::toModel).toList();
+        return Flux.from(entities.map(e -> entityToModel(e, exchange)));
     }
 }
