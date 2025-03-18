0d6a795b8361b50fc0e0859bb540600468204428
K-MHOON <azmria117@gmail.com>
2025-03-18 23:13:34 +0900

    fix : 주문 서비스 리액티브 형식으로 변경

--- app/src/main/java/com/kmhoon/app/service/OrderService.java
+++ app/src/main/java/com/kmhoon/app/service/OrderService.java
@@ -1,48 +1,65 @@
 package com.kmhoon.app.service;
 
+import com.kmhoon.app.entity.CardEntity;
+import com.kmhoon.app.entity.ItemEntity;
 import com.kmhoon.app.entity.OrderEntity;
-import com.kmhoon.app.exceptions.ResourceNotFoundException;
+import com.kmhoon.app.model.Card;
 import com.kmhoon.app.model.NewOrder;
-import com.kmhoon.app.repository.OrderRepository;
-import com.kmhoon.app.repository.UserRepository;
+import com.kmhoon.app.repository.*;
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
-import org.apache.logging.log4j.util.Strings;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
-import java.util.Objects;
-import java.util.Optional;
+import java.util.List;
 import java.util.UUID;
+import java.util.function.BiFunction;
 
 @Service
 @RequiredArgsConstructor
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final UserRepository userRepository;
+    private final AddressRepository addressRepository;
+    private final CardRepository cardRepository;
+    private final ItemRepository itemRepository;
+    private final ShipmentRepository shipmentRepository;
+    private BiFunction<OrderEntity, List<ItemEntity>, OrderEntity> biOrderItems = (o, fi) -> o.setItems(fi);
 
-    @Transactional
-    public Optional<OrderEntity> addOrder(@Valid NewOrder newOrder) {
-        if(Strings.isEmpty(newOrder.getCustomerId())) {
-            throw new ResourceNotFoundException("부정확한 고객 ID 입니다.");
-        }
-        if(Objects.isNull(newOrder.getAddress()) || Strings.isEmpty(newOrder.getAddress().getId())) {
-            throw new ResourceNotFoundException("부정확한 주소 ID 입니다.");
-        }
-        if(Objects.isNull(newOrder.getCard()) || Strings.isEmpty(newOrder.getCard().getId())) {
-            throw new ResourceNotFoundException("부정확한 카드 ID 입니다.");
-        }
+    public Mono<OrderEntity> addOrder(@Valid Mono<NewOrder> newOrder) {
         return orderRepository.insert(newOrder);
     }
 
-    @Transactional(readOnly = true)
-    public Iterable<OrderEntity> getOrdersByCustomerId(@NotNull @Valid String customerId) {
-        return orderRepository.findByCustomerId(UUID.fromString(customerId));
+    public Mono<OrderEntity> updateMapping(@Valid OrderEntity orderEntity) {
+        return orderRepository.updateMapping(orderEntity);
     }
 
-    @Transactional(readOnly = true)
-    public Optional<OrderEntity> getByOrderId(String id) {
-        return orderRepository.findById(UUID.fromString(id));
+    public Flux<OrderEntity> getOrdersByCustomerId(String customerId) {
+        return orderRepository.findByCustomerId(UUID.fromString(customerId)).flatMap(order ->
+                Mono.just(order)
+                        .zipWith(userRepository.findById(order.getCustomerId()))
+                        .map(t -> t.getT1().setUserEntity(t.getT2()))
+                        .zipWith(addressRepository.findById(order.getAddressId()))
+                        .map(t -> t.getT1().setAddressEntity(t.getT2()))
+                        .zipWith(cardRepository.findById(order.getCardId() != null ? order.getCardId() : UUID.fromString("0a59ba9f-629e-4445-8129-b9bce1985d6a")).defaultIfEmpty(new CardEntity()))
+                        .map(t -> t.getT1().setCardEntity(t.getT2()))
+                        .zipWith(itemRepository.findByCustomerId(order.getCustomerId()).collectList(), biOrderItems)
+                );
     }
+
+    public Mono<OrderEntity> getByOrderId(String id) {
+        return orderRepository.findById(UUID.fromString(id)).flatMap(order ->
+                Mono.just(order)
+                .zipWith(userRepository.findById(order.getCustomerId()))
+                .map(t -> t.getT1().setUserEntity(t.getT2()))
+                        .zipWith(addressRepository.findById(order.getAddressId()))
+                        .map(t -> t.getT1().setAddressEntity(t.getT2()))
+                        .zipWith(cardRepository.findById(order.getCardId()))
+                        .map(t -> t.getT1().setCardEntity(t.getT2()))
+                        .zipWith(itemRepository.findByCustomerId(order.getCustomerId()).collectList(), biOrderItems)
+        );
+    }
+
 }
