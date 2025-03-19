880b7bd55344c9cdb43e214b24562aaf757301db
K-MHOON <azmria117@gmail.com>
2025-03-19 23:40:14 +0900

    fix : 결제 서비스 리액티브 형식으로 변경

--- app/src/main/java/com/kmhoon/app/service/PaymentService.java
+++ app/src/main/java/com/kmhoon/app/service/PaymentService.java
@@ -9,9 +9,8 @@ import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
+import reactor.core.publisher.Mono;
 
-import java.util.Optional;
 import java.util.UUID;
 
 @Service
@@ -21,12 +20,11 @@ public class PaymentService {
     private final PaymentRepository paymentRepository;
     private final OrderRepository orderRepository;
 
-    public Optional<AuthorizationEntity> authorize(@Valid PaymentReq paymentReq) {
-        return Optional.empty();
+    public Mono<AuthorizationEntity> authorize(@Valid Mono<PaymentReq> paymentReq) {
+        return Mono.empty();
     }
 
-    @Transactional(readOnly = true)
-    public Optional<AuthorizationEntity> getOrdersPaymentAuthorization(@NotNull String orderId) {
+    public Mono<AuthorizationEntity> getOrdersPaymentAuthorization(@NotNull String orderId) {
         return orderRepository.findById(UUID.fromString(orderId)).map(OrderEntity::getAuthorizationEntity);
     }
 }
