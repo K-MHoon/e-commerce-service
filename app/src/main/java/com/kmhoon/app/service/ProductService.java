747f21aa831ce176db04985e1ac0e2bf230a4261
K-MHOON <azmria117@gmail.com>
2025-03-20 23:34:36 +0900

    fix : 상품 서비스 리액티브 형식으로 변경

--- app/src/main/java/com/kmhoon/app/service/ProductService.java
+++ app/src/main/java/com/kmhoon/app/service/ProductService.java
@@ -1,27 +1,40 @@
 package com.kmhoon.app.service;
 
 import com.kmhoon.app.entity.ProductEntity;
+import com.kmhoon.app.entity.TagEntity;
 import com.kmhoon.app.repository.ProductRepository;
+import com.kmhoon.app.repository.TagRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
 
-import java.util.Optional;
+import java.util.List;
 import java.util.UUID;
+import java.util.function.BiFunction;
 
 @Service
 @RequiredArgsConstructor
 public class ProductService {
 
     private final ProductRepository productRepository;
+    private final TagRepository tagRepository;
+    private BiFunction<ProductEntity, List<TagEntity>, ProductEntity> productTagBiFun = (p, t) -> p.setTags(t);
 
-    @Transactional(readOnly = true)
-    public Iterable<ProductEntity> getAllProducts() {
-        return productRepository.findAll();
+    public Flux<ProductEntity> getAllProducts() {
+        return productRepository.findAll()
+                .flatMap(products ->
+                        Mono.just(products)
+                                .zipWith(tagRepository.findTagsByProductId(products.getId()).collectList())
+                                .map(t -> t.getT1().setTags(t.getT2()))
+                );
     }
 
-    @Transactional(readOnly = true)
-    public Optional<ProductEntity> getProduct(String id ){
-        return productRepository.findById(UUID.fromString(id));
+    public Mono<ProductEntity> getProduct(String id) {
+        Mono<ProductEntity> product = productRepository.findById(UUID.fromString(id))
+                .subscribeOn(Schedulers.boundedElastic());
+        Flux<TagEntity> tags = tagRepository.findTagsByProductId(UUID.fromString(id)).subscribeOn(Schedulers.boundedElastic());
+        return Mono.zip(product, tags.collectList(), productTagBiFun);
     }
 }
