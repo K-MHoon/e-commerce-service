ba3711e9daa616044dd4df463ab5c9dd4e4dfa30
K-MHOON <azmria117@gmail.com>
2025-03-22 21:11:25 +0900

    fix : 배송 서비스 리액티브 형식으로 변경

--- app/src/main/java/com/kmhoon/app/service/ShipmentService.java
+++ app/src/main/java/com/kmhoon/app/service/ShipmentService.java
@@ -5,9 +5,8 @@ import com.kmhoon.app.repository.ShipmentRepository;
 import jakarta.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
+import reactor.core.publisher.Flux;
 
-import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -16,8 +15,7 @@ public class ShipmentService {
 
     private final ShipmentRepository shipmentRepository;
 
-    @Transactional(readOnly = true)
-    public Iterable<ShipmentEntity> getShipmentByOrderId(@Min(value = 1L, message = "부정확한 선적 ID 입니다.") String id) {
-        return shipmentRepository.findAllById(List.of(UUID.fromString(id)));
+    public Flux<ShipmentEntity> getShipmentByOrderId(@Min(value = 1L, message = "부정확한 배송 ID 입니다.") String id) {
+        return shipmentRepository.getShipmentByOrderId(UUID.fromString(id));
     }
 }
