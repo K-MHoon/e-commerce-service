4e23f8c49fd87af9750b124cac964ddc174fa37f
K-MHOON <azmria117@gmail.com>
2025-03-28 22:00:09 +0900

    fix : 고객 컨트롤러 리액티브 형식으로 변경

--- app/src/main/java/com/kmhoon/app/controllers/CustomerController.java
+++ app/src/main/java/com/kmhoon/app/controllers/CustomerController.java
@@ -1,6 +1,7 @@
 package com.kmhoon.app.controllers;
 
 import com.kmhoon.app.CustomerApi;
+import com.kmhoon.app.exceptions.ResourceNotFoundException;
 import com.kmhoon.app.hateoas.AddressRepresentationModelAssembler;
 import com.kmhoon.app.hateoas.CardRepresentationModelAssembler;
 import com.kmhoon.app.hateoas.UserRepresentationModelAssembler;
@@ -10,9 +11,14 @@ import com.kmhoon.app.model.User;
 import com.kmhoon.app.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.util.List;
 import java.util.Optional;
@@ -30,37 +36,32 @@ public class CustomerController implements CustomerApi {
     private final UserService userService;
 
     @Override
-    public ResponseEntity<Void> deleteCustomerById(String id) {
-        userService.deleteCustomerById(id);
-        return accepted().build();
-    }
-
-    @Override
-    public ResponseEntity<List<Address>> getAddressesByCustomerId(String id) {
-        return userService.getAddressesByCustomerId(id)
-                .map(addressAssembler::toListModel)
-                .map(ResponseEntity::ok)
-                .orElse(notFound().build());
-    }
-
-    @Override
-    public ResponseEntity<List<User>> getAllCustomers() {
-        return ok(assembler.toListModel(userService.getAllCustomers()));
-    }
-
-    @Override
-    public ResponseEntity<Card> getCardByCustomerId(String id) {
-        return userService.getCardByCustomerId(id)
-                .map(cardAssembler::toModel)
-                .map(ResponseEntity::ok)
-                .orElse(notFound().build());
-    }
-
-    @Override
-    public ResponseEntity<User> getCustomerById(String id) {
+    public Mono<ResponseEntity<Void>> deleteCustomerById(String id, ServerWebExchange exchange) throws Exception {
         return userService.getCustomerById(id)
-                .map(assembler::toModel)
-                .map(ResponseEntity::ok)
-                .orElse(notFound().build());
+                .flatMap(c -> userService.deleteCustomerById(c.getId())
+                        .then(Mono.just(status(HttpStatus.ACCEPTED).<Void>build())))
+                .switchIfEmpty(Mono.just(notFound().build()));
+    }
+
+    @Override
+    public Mono<ResponseEntity<Flux<Address>>> getAddressesByCustomerId(String id, ServerWebExchange exchange) throws Exception {
+        return Mono.just(ok(userService.getAddressesByCustomerId(id)
+                .map(c -> addressAssembler.entityToModel(c, exchange))
+                .switchIfEmpty(Mono.error(new ResourceNotFoundException("고객 주소를 찾을 수 없습니다.")))));
+    }
+
+    @Override
+    public Mono<ResponseEntity<Flux<User>>> getAllCustomers(ServerWebExchange exchange) throws Exception {
+        return CustomerApi.super.getAllCustomers(exchange);
+    }
+
+    @Override
+    public Mono<ResponseEntity<Card>> getCardByCustomerId(String id, ServerWebExchange exchange) throws Exception {
+        return CustomerApi.super.getCardByCustomerId(id, exchange);
+    }
+
+    @Override
+    public Mono<ResponseEntity<User>> getCustomerById(String id, ServerWebExchange exchange) throws Exception {
+        return CustomerApi.super.getCustomerById(id, exchange);
     }
 }
